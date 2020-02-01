72ea266cfcc89d0889a499d88a1081d62af25bad
Yegor Bitensky <yegorbitensky@gmail.com>
2020-02-02 00:57:50 +0500

    add required fields to QuestioningForm and validate user data not the same

--- questioning/questioning_app/forms.py
+++ questioning/questioning_app/forms.py
@@ -23,12 +23,44 @@ class QuestioningForm(forms.ModelForm):
             'password': forms.HiddenInput(),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._set_required_fields()
+
+    def _set_required_fields(self):
+        for field_name in ('first_name', 'last_name', 'age', 'occupation'):
+            self.fields[field_name].required = True
+
+    def _check_same_user_exists(self, cleaned_data):
+        same_user_exists = (
+            User.objects
+            .filter(
+                first_name=cleaned_data.get('first_name'),
+                last_name=cleaned_data.get('last_name'),
+                age=cleaned_data.get('age'),
+                occupation=cleaned_data.get('occupation'),
+                email=cleaned_data.get('email'),
+                facebook_username=cleaned_data.get('facebook_username'),
+            )
+            .exists()
+        )
+        if same_user_exists:
+            raise forms.ValidationError(
+                'Пользователь с такими данными уже существует. '
+                'Вы можете скорректировать данные или завершить сессию.'
+            )
+
     def full_clean(self):
         timestamp = timezone.now().strftime('%y%m%d%H%M%S%f')
         self.data = self.data.copy()
         self.data['username'] = timestamp
         self.data['password'] = timestamp
         return super().full_clean()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        self._check_same_user_exists(cleaned_data)
+        return cleaned_data
 
     def save(self, *args, **kwargs):
         user = super().save(*args, **kwargs)
